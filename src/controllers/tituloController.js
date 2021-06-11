3966d6e890f8a79be4e87765db1a32c758624b86
Ana Carolina <carolina.oliveiralima9@gmail.com>
2021-06-11 00:21:36 -0300

    Crud do Repositório Finalizado

--- src/controllers/tituloController.js
+++ src/controllers/tituloController.js
@@ -20,7 +20,7 @@ const criaTitulo = async (request, response) => {
     }
 
 
-const tituloJaExiste = await tituloModel.findOne({ nome: request.body.nome })
+    const tituloJaExiste = await tituloModel.findOne({ nome: request.body.nome })
     if (tituloJaExiste) {
         return response.status(409).json({ error: "Titulo já cadastrado!" })
     }
@@ -41,20 +41,61 @@ const mostraTitulos = async (request, response) => {
     return response.status(200).json(titulos)
 }
 
-//const mostraTitulosMarvel = async (request, response) => {
-//     const titulos = await titulo.find().populate("estudio")
-//     const titulosFiltrados = titulos.filter(titulo => titulo.estudio.nome == "Marvel")
-//     return response.status(200).json(titulosFiltrados)
-// }
-
-const mostraTitulosGhibli = async (request, response)=>{
-    const titulos = await   tituloModel.find().populate("estudio")
-    const titulosFiltrados = titulos.filter(titulo=>titulo.estudio.nome=="Ghibli")
-
+const mostraTitulosMarvel = async (request, response) => {
+    const titulos = await tituloModel.find().populate("estudio") //populate permite preencher a propriedade com os dados do estudio em vez de mostrar apenas o identificador
+    const titulosFiltrados = titulos.filter(titulo => titulo.estudio.nome == "Marvel")
     return response.status(200).json(titulosFiltrados)
 }
+
+const mostraTitulosGhibli = async (request, response) => {
+    const titulos = await tituloModel.find().populate("estudio")
+    const titulosFiltrados = titulos.filter(titulo => titulo.estudio.nome == "Ghibli")
+    return response.status(200).json(titulosFiltrados)
+}
+
+const mostraTitulosPixar = async (request, response) => {
+    const titulos = await tituloModel.find().populate("estudio")
+    const titulosFiltrados = titulos.filter(titulo => titulo.estudio.nome == "Pixar")
+    return response.status(200).json(titulosFiltrados)
+}
+
+const atualizaTitulo = async (request, response) => {
+    const encontraTitulo = await tituloModel.findById(request.params.id)
+    if (encontraTitulo == null) {
+        return response.status(404).json({ message: "Titulo nao encontrado" })
+    }
+    if (request.body.nome != null) {
+        encontraTitulo.nome = request.body.nome
+    }
+    try {
+        const tituloAtualizado = await encontraTitulo.save()
+        response.status(200).json(tituloAtualizado)
+    }
+    catch (err) {
+        response.status(500).json({ message: err.message })
+    }
+}
+
+const deletaTitulo = async (request, response) => {
+    const encontraTitulo = await tituloModel.findById(request.params.id)
+    if (encontraTitulo == null) {
+        return response.status(404).json({ message: "Filme não encontrado" })
+    }
+    try {
+        await encontraTitulo.remove()
+        response.status(200).json({ message: "O filme foi deletado com sucesso" })
+    }
+    catch (err) {
+        response.status(500).json({ message: err.message })
+    }
+}
+
 module.exports = {
     criaTitulo,
     mostraTitulos,
-    mostraTitulosGhibli
+    mostraTitulosMarvel,
+    mostraTitulosGhibli,
+    mostraTitulosPixar,
+    atualizaTitulo,
+    deletaTitulo
 }
--- src/routes/titulos.routes.js
+++ src/routes/titulos.routes.js
@@ -5,7 +5,12 @@ const titulosController = require("../controllers/tituloController")
 router.post("/", titulosController.criaTitulo)
 
 router.get("/", titulosController.mostraTitulos)
+router.get("/marvel", titulosController.mostraTitulosMarvel)
 router.get("/ghibli", titulosController.mostraTitulosGhibli)
+router.get("/pixar", titulosController.mostraTitulosPixar)
 
+router.patch("/:id", titulosController.atualizaTitulo)
+
+router.delete("/:id", titulosController.deletaTitulo)
 
 module.exports = router
